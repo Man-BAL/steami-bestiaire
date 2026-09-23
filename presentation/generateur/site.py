f98eb0e3fa4ee1535cc177ea935f2dd8d90e47ff
Manon <sebastien.nedjar@gmail.com>
2026-09-23 12:27:23 +0200

    Favicon : l'abeille des sentinelles

--- presentation/generateur/site.py
+++ presentation/generateur/site.py
@@ -415,6 +415,8 @@ html=f'''<!doctype html>
 <html lang="fr"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bestiaire STeaMi</title>
 <meta name="description" content="Des animaux-sentinelles qui embarquent une carte STeaMi pour observer le vivant et mesurer nos conditions de vie.">
+<link rel="icon" href="favicon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="favicon-180.png">
 <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;600;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
 <style>{css}</style></head>
